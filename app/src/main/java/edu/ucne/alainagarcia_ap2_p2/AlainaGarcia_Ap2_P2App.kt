5969519fbcf202be5281f6203de09cf739d28e3d
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-18 19:16:15 -0400

    Configura inyeccion dependencias con Hilt

---
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p2/AlainaGarcia_Ap2_P2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.alainagarcia_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AlainaGarcia_Ap2_P2App : Application(){
+
+}
--- app/src/main/java/edu/ucne/alainagarcia_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p2/MainActivity.kt
@@ -11,37 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.alainagarcia_ap2_p2.ui.theme.AlainaGarcia_Ap2_P2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AlainaGarcia_Ap2_P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlainaGarcia_Ap2_P2Theme {
-        Greeting("Android")
     }
 }
