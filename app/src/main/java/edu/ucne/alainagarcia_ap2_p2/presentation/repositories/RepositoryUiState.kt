bf87d1b0b76261ecc2a7a80a7e485828a98243de
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-19 15:40:23 -0400

    UiState realizado

--- app/src/main/java/edu/ucne/alainagarcia_ap2_p2/presentation/repositories/RepositoryUiState.kt
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p2/presentation/repositories/RepositoryUiState.kt
@@ -1,2 +1,12 @@
 package edu.ucne.alainagarcia_ap2_p2.presentation.repositories
 
+import edu.ucne.alainagarcia_ap2_p2.data.remote.dto.RepositoryDto
+
+data class RepositoryUiState (
+    val name: String = "",
+    val descripcion: String = "",
+    val html_url: String = "",
+    val isLoading: Boolean = false,
+    val repository: List<RepositoryDto> = emptyList(),
+    val errorMessage: String? = null,
+)
