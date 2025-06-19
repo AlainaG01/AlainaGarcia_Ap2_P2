0f79908dc01f314447c5e9d9944fa68cf406ee95
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-19 15:42:07 -0400

    MainActivity modificado

--- app/src/main/java/edu/ucne/alainagarcia_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.alainagarcia_ap2_p2.presentation.navigation.HostNavigation
 import edu.ucne.alainagarcia_ap2_p2.ui.theme.AlainaGarcia_Ap2_P2Theme
 
 @AndroidEntryPoint
@@ -21,7 +24,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AlainaGarcia_Ap2_P2Theme {
+                val nav = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        HostNavigation(nav)
+                    }
 
+                }
             }
         }
     }
