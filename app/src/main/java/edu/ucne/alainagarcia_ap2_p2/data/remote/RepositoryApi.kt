595ad4c654a8db0b14bf5c5c7b03f6aa3f988ddc
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-09 21:14:33 -0400

    Agregando el endpoint para listar los contribuidores

--- app/src/main/java/edu/ucne/alainagarcia_ap2_p2/data/remote/RepositoryApi.kt
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p2/data/remote/RepositoryApi.kt
@@ -1,5 +1,6 @@
 package edu.ucne.alainagarcia_ap2_p2.data.remote
 
+import edu.ucne.alainagarcia_ap2_p2.data.remote.dto.ContributorDto
 import edu.ucne.alainagarcia_ap2_p2.data.remote.dto.RepositoryDto
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -7,4 +8,11 @@ import retrofit2.http.Path
 interface GitHubApi {
     @GET("users/{username}/repos")
     suspend fun listRepos(@Path("username") username: String): List<RepositoryDto>
+
+    @GET("repos/{owner}/{repo}/contributors")
+    suspend fun listContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): List<ContributorDto>
+
 }
